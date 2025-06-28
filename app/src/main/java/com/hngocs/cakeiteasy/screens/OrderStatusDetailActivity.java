488811c520325d61b9dbf2ee191b22d6b4f54c57
Gia Huy <giahuy7112904@gmai.com>
2025-06-28 22:40:08 +0700

    4 - card product xml, login xml

--- app/src/main/java/com/hngocs/cakeiteasy/screens/OrderStatusDetailActivity.java
+++ app/src/main/java/com/hngocs/cakeiteasy/screens/OrderStatusDetailActivity.java
@@ -16,7 +16,7 @@ public class OrderStatusDetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_order_status_detail);
+        setContentView(R.layout.activity_order_status_detail2);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
